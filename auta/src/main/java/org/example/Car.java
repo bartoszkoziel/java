41c6cc0f4235441956dd6c2652c361b2e312bb24
bartoszkoziel <83342505+bartoszkoziel@users.noreply.github.com>
2023-10-27 12:22:41 +0200

    Add files via upload

--- auta/src/main/java/org/example/Car.java
+++ auta/src/main/java/org/example/Car.java
@@ -3,15 +3,9 @@ package org.example;
 import java.util.ArrayList;
 
 class Car{
-    private String model;
-    private int rok;
-    private ArrayList<Car> poduszki;
-    private String kolor;
+    public String model;
+    public int rok;
+    public ArrayList<Car> poduszki;
+    public String kolor;
 
-    public Car(String model, int rok, ArrayList<Car> poduszki, String kolor) {
-        this.model = model;
-        this.rok = rok;
-        this.poduszki = poduszki;
-        this.kolor = kolor;
-    }
 }
--- auta/src/main/java/org/example/Main.java
+++ auta/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
+
 import static spark.Spark.*;
+import com.google.gson.Gson;
 import spark.Request;
 import spark.Response;
 import org.example.Car;
@@ -8,13 +10,11 @@ import org.example.Airbag;
 import java.util.ArrayList;
 
 public class Main {
-    private ArrayList<Car> carlist;
-
-    public void setCarlist(ArrayList<Car> carlist) {
-        this.carlist = carlist;
+    private static ArrayList<Car> carlist = new ArrayList<Car>();
+    public static void setCarlist(ArrayList<Car> carlist) {
+        Main.carlist = carlist;
     }
-
-    public ArrayList<Car> getCarlist() {
+    public static ArrayList<Car> getCarlist() {
         return carlist;
     }
 
@@ -27,12 +27,27 @@ public class Main {
 
         System.out.printf("Hello and welcome!");
         post("/add", (req, res) -> handleAdd(req, res));
+        get("/json", (req, res) -> handleJson(req, res));
+
     }
 
     static String handleAdd(Request req, Response res) {
         String responsetext = req.body();
-        res.type("application/json");
+        System.out.println("RESPONSETEXT: " + responsetext);
 
+        Gson gson = new Gson();
+        Car car = gson.fromJson(responsetext, Car.class);
+
+        ArrayList<Car> temp = getCarlist();
+        temp.add(car);
+        setCarlist(temp);
+
+        res.type("application/json");
         return responsetext;
+    }
+
+    static String handleJson(Request req, Response res) {
+        Gson gson = new Gson();
+        return gson.toJson(getCarlist(), ArrayList.class );
     }
 }
